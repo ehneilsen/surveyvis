Fix scaling of sky brightness maps

python/surveyvis/app/sched_maps.py
@@ -7,6 +7,7 @@ from surveyvis.plot.SphereMap import (
     HorizonMap,
     Planisphere,
     MollweideMap,
+    make_zscale_linear_cmap,
 )
 
 from surveyvis.collect import read_scheduler, read_conditions
@@ -187,6 +188,9 @@ def make_scheduler_map_figure(
     def switch_value(attrname, old, new):
         hp_values = scheduler_healpix_maps[new]
 
+        # Rescale the color map for the new data
+        new_cmap = make_zscale_linear_cmap(hp_values)
+
         new_ds = arm.make_healpix_data_source(
             hp_values,
             nside=nside,
@@ -200,13 +204,6 @@ def make_scheduler_map_figure(
         # Replace the data to be shown
         hp_ds.data = new_data
 
-        # Rescale the color map for the new data
-        new_cmap = bokeh.transform.linear_cmap(
-            "value",
-            "Inferno256",
-            np.nanmin(new_data["value"]),
-            np.nanmax(new_data["value"]),
-        )
         arm_hp_glyph.fill_color = new_cmap
         arm_hp_glyph.line_color = new_cmap
         pla_hp_glyph.fill_color = new_cmap

python/surveyvis/munge/monkeypatch_rubin_sim.py
@@ -74,7 +74,8 @@ class Core_scheduler(rubin_sim.scheduler.schedulers.core_scheduler.Core_schedule
 
         maps = OrderedDict()
         for band in conditions.skybrightness.keys():
-            maps[f"{band}_sky"] = conditions.skybrightness[band]
+            maps[f"{band}_sky"] = deepcopy(conditions.skybrightness[band])
+            maps[f"{band}_sky"][maps[f"{band}_sky"]<-1e30] = np.nan
 
         basis_functions = self.get_basis_functions(survey_index, conditions)
 

python/surveyvis/plot/SphereMap.py
@@ -10,6 +10,7 @@ import bokeh.plotting
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astropy.time import Time
+import astropy.visualization
 
 from rubin_sim.utils.Site import Site
 from rubin_sim.utils import calcLmstLast
@@ -903,12 +904,7 @@ class SphereMap:
         self.healpix_data = data_source
 
         if cmap is None:
-            cmap = bokeh.transform.linear_cmap(
-                "value",
-                "Inferno256",
-                np.nanmin(data_source.data["value"]),
-                np.nanmax(data_source.data["value"]),
-            )
+            cmap = make_zscale_linear_cmap(data_source.data["value"])
 
         self.healpix_cmap = cmap
 
@@ -1463,3 +1459,11 @@ class ArmillarySphere(MovingSphereMap):
         self.figure = bokeh.layouts.column(
             self.plot, self.sliders["alt"], self.sliders["az"], self.sliders["lst"]
         )
+
+
+def make_zscale_linear_cmap(values, field_name='value', palette='Inferno256', *args, **kwargs):
+    zscale_interval = astropy.visualization.ZScaleInterval(*args, **kwargs)
+    scale_limits = zscale_interval.get_limits(values)
+    cmap = bokeh.transform.linear_cmap(field_name, palette, scale_limits[0], scale_limits[1])
+    return cmap
+    